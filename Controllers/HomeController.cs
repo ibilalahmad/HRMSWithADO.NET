01b70a9e4e5f62e9cecd322ccd6a72c118003ae5
Bilal Ahmad <89376685+ibilalahmad@users.noreply.github.com>
2025-08-27 10:55:17 +0500

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,10 +27,7 @@ namespace AhmadHRManagementSystem.Controllers
         }
 
         [AuthorizeRole("Admin", "User")]
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
